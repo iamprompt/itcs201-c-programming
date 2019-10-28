78b80b06dcf5304a0c0f4a03d45af2b7609bfe9c
wearedprompt <wearedprompt@gmail.com>
2019-10-28 14:52:45 +0700

    Lecture 9

---
+++ 20191028 - LAB 9/Lecture/sum2d10.c
@@ -0,0 +1,33 @@
+// Sum all elements in 2D arrays (n x m) Only Odd Column
+#include <stdio.h>
+
+int main()
+{
+  int nrow, ncol;
+  scanf("%d %d", &nrow, &ncol);
+
+  int myarr[nrow][ncol];
+  int sum = 0;
+
+  for (int row = 0; row < nrow; row++)
+  {
+    for (int col = 0; col < ncol; col++)
+    {
+      scanf("%d", &myarr[row][col]);
+    }
+  }
+
+  for (int row = 0; row < nrow; row++)
+  {
+    for (int col = 0; col < ncol; col++)
+    {
+      if (myarr[row][col] > 10)
+      {
+        sum += myarr[row][col];
+      }
+    }
+  }
+  printf("%d\n", sum);
+
+  return 0;
+}
