e9405f56e8fd68e22ba7963e5de79fb7419b7e57
wearedprompt <wearedprompt@gmail.com>
2019-11-11 23:03:23 +0700

    LAB 11 Finish

--- 20191111 - LAB 11/Q1.c
+++ 20191111 - LAB 11/Q1.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 
-float thb2usd(){
-  
+// Define USD & JPY
+float THBtoUSD = 32.86;
+float THBtoJPY = 0.29;
+
+float thb2usd(float thb)
+{
+    float usd = thb / THBtoUSD;
+    return usd;
 }
 
-float thb2jpy(){
-
+float thb2jpy(float thb)
+{
+    float jpy = thb / THBtoJPY;
+    return jpy;
 }
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
 
+    float thb[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%f", &thb[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%.2f %.2f\n", thb2usd(thb[i]), thb2jpy(thb[i]));
+    }
+
+    return 0;
 }
---
+++ 20191111 - LAB 11/Q4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int compute_gcd(int index, int n1, int n2)
+{
+    int gcd;
+    n1 = (index == 0) ? n2 : n1;
+    int max = (n1 > n2) ? n1 : n2;
+
+    for (int i = max; i >= 0; i--)
+    {
+        if (n1 % i == 0 && n2 % i == 0)
+        {
+            gcd = i;
+            break;
+        }
+    }
+
+    return gcd;
+}
+
+int main()
+{
+    int n, gcd;
+    scanf("%d", &n);
+
+    int numarr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &numarr[i]);
+        gcd = compute_gcd(i, gcd, numarr[i]);
+        // printf("gcd of %d and %d is %d\n", gcd, numarr[i], gcd);
+    }
+
+    printf("%d\n", gcd);
+
+    return 0;
+}
