17f13f8a8d3b907a4b87e6a7c2a00450a1f55e0c
Supakarn Laorattanakul <wearedprompt@gmail.com>
2019-11-18 20:06:10 +0700

    Q4 LAB 12 Finished

--- 20191118 - LAB 12/Q4.c
+++ 20191118 - LAB 12/Q4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int intersect(int *arr1, int *arr2, int n, int *out);
+void sort(int *arr, int n);
 
 int main()
 {
@@ -17,26 +18,79 @@ int main()
         scanf("%d", &arr2[i]);
     }
 
+    // Count Base on arr1
     int out[n];
 
-    intersect(arr1, arr2, n, out);
+    sort(arr1, n);
+    int its = intersect(arr1, arr2, n, out);
+
+    for (int i = 0; i < its; i++)
+    {
+        printf("%d ", out[i]);
+    }
 
     return 0;
 }
 
 int intersect(int *arr1, int *arr2, int n, int *out)
 {
-    int count = 0;
+    int count[n];
+    // ARR1 Controller
     for (int i = 0; i < n; i++)
-    {   
+    {
+        count[i] = 0;
+        // ARR2 Controller
         for (int j = 0; j < n; j++)
         {
             if (*(arr1 + i) == *(arr2 + j))
             {
-                printf("Heyyyyyy!!!\n");
-                *(out + count) = *(arr1 + i);
+                count[i]++;
+                *(arr2 + j) = "";
+                // printf("Found %d\n", count[i]);
             }
-        }  
-        printf("Hey123!!!\n");  
+        }
     }
+
+    int its = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // printf("%d ", count[i]);
+        if (count[i] > 0)
+        {
+            *(out + its) = *(arr1 + i);
+            its++;
+        }
+    }
+    return its;
+}
+
+void sort(int *arr, int n)
+{
+    int count;
+    do
+    {
+        count = 0;
+        for (int i = 0; i < n; i++)
+        {
+
+            int j = (i == 0) ? i : i - 1;
+            if (*(arr + j) > *(arr + i))
+            {
+                int temp = *(arr + i);
+                *(arr + i) = *(arr + j);
+                *(arr + j) = temp;
+            }
+            else
+            {
+                count++;
+                // printf("Count order %d => %d %d\n", count, arr[j], arr[i]);
+            }
+        }
+
+        /* for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n"); */
+    } while (count != n);
 }
